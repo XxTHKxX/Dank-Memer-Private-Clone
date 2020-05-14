41c65c1668b327fd861c42ed509fdc292752162a
Đức Anh Trần <tranducanh2002@gmail.com>
2020-05-15 00:08:56 +0700

    a fucking " sign

--- Dmemer.py
+++ Dmemer.py
@@ -38,7 +38,7 @@ async def init(ctx):
 async def list(ctx):
 	for guild in bot.guilds:
 		for member in guild.members:
-			cur.execute(f"SELECT * FROM data WHERE id = {member.id})
+			cur.execute(f"SELECT * FROM data WHERE id = {member.id}")
 			record = cur.fetchall()
 			for row in record:
 				await ctx.send("ID: ", row[0])
