03792b2a07e147a009182b4366e5aa66bd08c560
Đức Anh Trần <tranducanh2002@gmail.com>
2020-06-06 06:33:55 +0700

    return to previous state

--- Dmemer.py
+++ Dmemer.py
@@ -39,17 +39,14 @@ def connectsql():
 @bot.command()
 async def init(ctx):
     connectsql() #Connect to the database
-    cur.execute("CREATE TABLE data (id BIGINT, username TEXT, amount INTEGER, lastdaily TEXT)") #Start the database creation process
+    cur.execute("CREATE TABLE data (id BIGINT, username TEXT, amount INTEGER)") #Start the database creation process
     for guild in bot.guilds: #Looping though all servers
 	      for member in guild.members: #Looping though all members
 	      	if member.bot == True:
 	      		pass #Checking if a user is a bot, if True, skip this user
 	      	else:
 	          targetname = repr(member.name + "#" + member.discriminator)
-	          cur.execute("SELECT CURRENT_DATE")
-	          currenttime = str(cur.fetchone())
-	          print(currenttime)
-	          cur.execute(f"INSERT INTO data (id, username, lastdaily, amount) VALUES ({member.id}, {targetname}, {currenttime}, 5000)") #Adding member to database
+	          cur.execute(f"INSERT INTO data (id, username, amount) VALUES ({member.id}, {targetname}, 5000)") #Adding member to database
 	          await ctx.send(f"Member {targetname} has been added to the database") #Reporting to the user on who get added
 	          time.sleep(0.75) #Waiting 0.75 seconds to bypass discord rate limit
 	      	
@@ -88,8 +85,7 @@ async def daily(ctx):
 	redeemtime = str(cur.fetchone())
 	cur.execute(f"SELECT * FROM data WHERE id = {userid}")
 	data = cur.fetchone()
-	currentbal = data[3]
-	lastredeem = data[2]
+	currentbal = data[2]
 	if redeemtime == lastredeem:
 		await ctx.send("Sorry! you've already redeemed your daily box today, try again tomorrow")
 	else:
@@ -109,12 +105,12 @@ async def rob(ctx, target : discord.Member):
 	row = cur.fetchone() #Get data of the victim
 	if row == None:
 		await ctx.send("Unable to find target") #Report if the user is not found
-	targetbal = row[3] #Saving the balance data of the victim
+	targetbal = row[2] #Saving the balance data of the victim
 	cur.execute(f"SELECT * FROM data WHERE id = {attackerid}") #Getting the data of the attacker
 	row = cur.fetchone() #Get data of attacker
 	if row == None:
 		await ctx.send("Unable to find your profile, are you sure you're enrolled?") #Report if user not found
-	attackerbal = row[3]	#Saving data of attacker
+	attackerbal = row[2]	#Saving data of attacker
 	successmin = 40 #Minimum success number
 	roll1 = random.randint(1,100) # Generate first number
 	if roll1 >= successmin: #Check if the attacker succeed
