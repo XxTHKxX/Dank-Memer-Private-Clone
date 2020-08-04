e9cc3eee481a97463fc58514c60f93b1d9cf5b9b
Đức Anh Trần <tranducanh2002@gmail.com>
2020-08-04 20:33:12 +0700

    fuk

--- Dmemer.py
+++ Dmemer.py
@@ -133,7 +133,7 @@ async def rob(ctx, target : discord.Member):
 	connectsql() #Connect to database
 	attackerid = ctx.author.id #Get the ID of the attacker
 	user = target #Get ID of the victim
-	if user == attackerid:
+	if user.id == attackerid:
 		ctx.send("You cannot rob yourself you dumb fuck")
 		return
 	cur.execute(f"SELECT * FROM data WHERE id = {user.id}")
